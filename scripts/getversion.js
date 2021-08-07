523428a61569c932a948b02ee446f45ce81c7fb9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-07 18:31:11 +0200

    Replaced XMLHttpRequest with fetch()

--- scripts/getversion.js
+++ scripts/getversion.js
@@ -1,13 +1,8 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    console.log("HTTP Result:\n\n" + xmlHttp.responseText);
-    return xmlHttp.responseText;
+async function getVersion(url) {
+    let response = await fetch(vUrl);
+    let data = await response.text();
+    document.getElementById("downloadLink").innerHTML = "Download Gavilya <span class=\"font-normal hidden sm:inline-block\">(v" + data + ")</span>";
 }
 
 const vUrl = "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/Gavilya/Version.txt";
-
-var response = httpGet(vUrl);
-document.getElementById("downloadLink").innerHTML = "Download Gavilya <span class=\"font-normal hidden sm:inline-block\">(v" + response + ")</span>";
+getVersion(vUrl);
