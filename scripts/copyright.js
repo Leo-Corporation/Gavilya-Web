ad8510b4cb0cddf9c68e0b994c8b969bec8ffed7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-22 10:11:06 +0200

    Updated footer

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,4 +1,4 @@
-let p = document.getElementById("copyright-p");
+let p = document.getElementById("copyright");
 let year = new Date().getFullYear();
 console.info(year);
 p.innerHTML = "© " + year + " Léo Corporation";
